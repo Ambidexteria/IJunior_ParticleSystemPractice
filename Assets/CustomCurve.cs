0e0279ab9783b8e01135129ee96599fa4251e356
Ambidexteria <43927380+Ambidexteria@users.noreply.github.com>
2024-10-22 17:11:58 +0300

    Initial commit

---
+++ Assets/CustomCurve.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCurve : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve _curve;
+}
